8d7d284d30f81e5e14b7a69cee2a7cc5b8153c60
George Padley <george.r.padley@gmail.com>
2017-03-14 14:30:57 +0000

    Figure Changes

--- Exercise 1/ralston_script.m
+++ Exercise 1/ralston_script.m
@@ -1,14 +1,34 @@
 clear;
 close all;
-tc = 150e-6;
-Vin = @(t) 3.5*exp(-t/tc);
+tc = 1100e-6;
+f = 1/tc;
+Vin = @(t) 4*sawtooth(2*pi*f*t);
 R = 0.5; L = 1.5*10^(-3);
 func =@(t, x) (Vin(t) - R*x)/L;
 x0 = 0;
 t0 = 0;
-tf = 0.02;
-h = 0.00001;
+tf = 0.003;
+h = 0.00000001;
 [t, x] = ralston(func,t0,x0,tf,h);
-vout = 3.5-R*x;
+vin = Vin(t);
+vout = vin-R*x;
+subplot(2,2,1);
 plot(t, vout);
-title 'Ralston Vin = 3.5V'
+grid on;
+title 'Ralston'
+subplot(2,2,2);
+[t, x] = midpoint(func,t0,x0,tf,h);
+vout = vin-R*x;
+plot(t, vout, 'g');
+grid on;
+title 'Midpoint'
+subplot(2,2,3);
+[t, x] = heun(func,t0,x0,tf,h);
+vout = vin-R*x;
+plot(t, vout, 'r');
+grid on;
+title 'Heun'
+subplot(2,2,4);
+plot(t, vin);
+title 'Vin(t) = Sawtooth T = 1100us'
+grid on;
